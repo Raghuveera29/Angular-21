37108383e888172177235843795e214fe5634e32
Raghuveera <raghuveer.s@propellente.com>
2026-02-26 19:14:17 +0530

    first push

---
+++ src/app/Day_1/product-list/product-list.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.scss',
+})
+export class ProductList {
+  products = [
+    { id: 1, name: 'laptop', price: 3000, stock: true },
+    { id: 2, name: 'computer', price: 10000, stock: false },
+    { id: 3, name: 'apple', price: 50000, stock: true },
+  ];
+
+  selectProduct(p: any) {
+    console.log('selected', p.name);
+  }
+
+  trackById(index: number, item: any) {
+    return item.id;
+  }
+
+  addProduct() {
+    this.products.push({
+      id: Date.now(),
+      name: 'macbook',
+      price: 7000,
+      stock: true,
+    });
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProductList } from './Day_1/product-list/product-list';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductList,
+  },
+];
